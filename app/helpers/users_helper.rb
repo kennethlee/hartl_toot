e3734104d8adf881e4dfa733c6a1f37be2f0dc00
Kenneth Lee <kennyxlee@gmail.com>
2015-07-01 20:10:25 -0700

    add gravatar_for method (that doesn't work)

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,9 @@
 module UsersHelper
+  # returns the gravatar for the given user.
+  # p.s.: this shit doesn't work.
+  # def gravatar_for(user)
+  #   gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+  #   gravatar_url = "http://securegravatar.com/avatar/#{gravatar_id}"
+  #   image_tag(gravatar_url, alt: user.name, class: "gravatar")
+  # end
 end
